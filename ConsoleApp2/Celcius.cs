5e959a711a83663e6ffd019c621c00385398ad38
RasulRustamli <rasular@code.edu.az>
2023-04-12 14:26:47 +0400

    Add project files.

---
+++ ConsoleApp2/Celcius.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Celcius
+    {
+        public double Degree { get; set; }
+        public Celcius(double degree)
+        {
+            Degree = degree;
+        }
+        public static implicit operator Celcius(Kelvin kelvin)
+        {
+            return new Celcius(kelvin.Degree-273);
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kelvin kelvin = new Kelvin(290);
+            Celcius celcius = kelvin;
+            Console.WriteLine(celcius.Degree);
+        }
+    }
+}
